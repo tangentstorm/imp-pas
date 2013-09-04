762c134843d6a6254aa12605ab11470351e6e08b
tangentstorm <michal.wallace@gmail.com>
2013-09-04 00:48:53 -0500

    basic skeleton

---
+++ imp.pas
@@ -0,0 +1,8 @@
+{ just a top-level executable for implish }
+
+{$mode objfpc}
+program imp;
+uses implish;
+begin
+  implish.repl
+end.
---
+++ implish.pas
@@ -0,0 +1,16 @@
+{$mode objfpc}
+unit implish;
+interface uses variants, classes, sysutils;
+{ ------------------------------------------------------------ }
+
+procedure repl;
+
+{ ------------------------------------------------------------ }
+implementation
+
+procedure repl;
+  begin
+  end;
+  
+begin
+end.
