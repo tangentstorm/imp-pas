861062e67f1c99debe30afe0db937e74ece7dd24
tangentstorm <michal.wallace@gmail.com>
2013-09-07 23:46:26 -0500

    TRule -> TRuleID

--- imprex.pas
+++ imprex.pas
@@ -6,23 +6,23 @@ interface
 type
   TCharGen = function : char;
   TCharSet = set of char;
-  TRule	   = cardinal;
+  TRuleID	   = cardinal;
 
 //-- primitive rule constructors -------------------------------
 
 // nul : matches the empty string. (always matches)
-function nul : TRule;
+function nul : TRuleID;
 
 // eoi : matches the end of input.
-function eoi : TRule;
+function eoi : TRuleID;
 
 // any : matches any single character in the given set.
-function any( s : TCharSet ) : TRule;
+function any( s : TCharSet ) : TRuleID;
 
 // lit : matches a literal string.
 // As a convenience when using the combinators below,
 // you can also just pass the raw string.
-function lit( s : string ) : TRule;
+function lit( s : string ) : TRuleID;
 
 //-- combinators -----------------------------------------------
 
@@ -30,7 +30,7 @@ function lit( s : string ) : TRule;
 // Succeeds if and only if at least one of the sub-rules succeeds.
 // This is like separating sub-patterns by '|' in a regular expression.
 // The name is short for "alternative".
-function alt( rules : array of const ) : TRule;
+function alt( rules : array of const ) : TRuleID;
 
 // seq : match a specific sequence of rules, in order.
 // Succeeds if and only if all of the sub-rules succeed.
@@ -39,28 +39,28 @@ function alt( rules : array of const ) : TRule;
 // an 'array of const'. These are implicitly converted
 // into a sequence, so most likely, the only time you really
 // need to use 'seq' directly is inside an 'alt' constructor.
-function seq( rules : array of const ) : TRule;
+function seq( rules : array of const ) : TRuleID;
 
 // rep : matches a rule repeatedly.
 // This is the 'kleene plus' from formal language theory.
 // It works like like the '+' character in a regular expression.
-function rep( rules : array of const ) : TRule;
+function rep( rules : array of const ) : TRuleID;
 
 // neg : negate a match rule.
 // This implements a logical 'not' operator: if the underlying
 // pattern succeeds, then this rule will fail, and vice versa.
-function neg( rules : array of const ) : TRule;
+function neg( rules : array of const ) : TRuleID;
 
 // opt : optionally match a rule. Always succeeds.
 // This works like the '?' character in a regular expression.
 // opt([x,y,z]) is the same as alt(seq([x,y,z]), nul)
-function opt( rules : array of const ) : TRule;
+function opt( rules : array of const ) : TRuleID;
 
 // orp : optionally repeat a sequence of rules. Always succeeds.
 // This is the 'kleene star' from formal language theory.
 // It works like the '*' character in a regular expression.
 // orp([x,y,z]) is the same as opt(rep([x,y,z]))
-function orp( rules : array of const ) : TRule;
+function orp( rules : array of const ) : TRuleID;
 
 //-- named rules -----------------------------------------------
 
@@ -82,12 +82,12 @@ function orp( rules : array of const ) : TRule;
 // ... etc.
 
 // def : define a named rule.
-function def( name : string; rules : array of const ) : TRule;
+function def( name : string; rules : array of const ) : TRuleID;
 
 // sub : invoke a named rule.
 // To allow mutually recursive rules, a name can be referenced
 // before it has been defined.
-function sub( name : string ) : TRule;
+function sub( name : string ) : TRuleID;
 
 //-- action and event system -----------------------------------
 
@@ -97,13 +97,13 @@ function sub( name : string ) : TRule;
 // behavior when a match succeeds. This is used internally,
 // and can be used to create new generic action rules (like
 // the tree-building rules in the next section).
-function act( action : cardinal; rules : array of const ) : TRule;
+function act( action : cardinal; rules : array of const ) : TRuleID;
 
 // TODO : evt : trigger a callback before and after a match.
 // These could be used either to trigger immediate actions, or
 // as predicates to control the flow of grammar.
 //
-// function evt( e : TTokenCallback; rules : array of const ) : TRule;
+// function evt( e : TTokenCallback; rules : array of const ) : TRuleID;
 // procedure hook( e : TTokenCallback );
 
 //-- token trees -----------------------------------------------
@@ -113,23 +113,23 @@ function act( action : cardinal; rules : array of const ) : TRule;
 // syntax trees.
 
 // tok : define a rule that generates a token when matched.
-function tok( name : string; rules : array of const ) : TRule;
+function tok( name : string; rules : array of const ) : TRuleID;
 
 // skip : this is exactly the same as 'token', except that the
 // generated token will be marked as hidden. This is intended
 // for matching comments and whitespace.
-function skip( name : string; rules : array of const ) : TRule;
+function skip( name : string; rules : array of const ) : TRuleID;
 
 // node : define a rule that generates a node in the abstract
-// syntax tree when matched. 
-function node( name : string; rules : array of const ) : TRule;
+// syntax tree when matched.
+function node( name : string; rules : array of const ) : TRuleID;
 
 // These next rules alter the tree-building process in various
 // ways, or allow re-arranging the tree.
 
 // hide : match a sequence of rules, but do not include them in
 // the generated tree. This like the '!' operator in ANTLR 3.
-function hide(rules : array of const) : TRule;
+function hide(rules : array of const) : TRuleID;
 
 // lift : promote the matched sequence to the head of the
 // current node. This is like the '^' operator in ANTLR 3.
@@ -137,10 +137,10 @@ function hide(rules : array of const) : TRule;
 // start of the list. Generally you would only apply this to
 // a single token, but you can promote a whole sequence if
 // you really want.
-function lift(rules : array of const) : TRule;
+function lift(rules : array of const) : TRuleID;
 
 // virt : emit a virtual token of length 0. Always succeeds.
-function virt( name : string ) : TRule;
+function virt( name : string ) : TRuleID;
 
 implementation
 
@@ -148,75 +148,75 @@ function nul;
   begin
   end;
 
-function eoi : TRule;
+function eoi : TRuleID;
   begin
   end;
 
-function any( s : TCharSet ) : TRule;
+function any( s : TCharSet ) : TRuleID;
   begin
   end;
 
-function lit( s : string ) : TRule;
+function lit( s : string ) : TRuleID;
   begin
   end;
 
-function alt( rules : array of const ) : TRule;
+function alt( rules : array of const ) : TRuleID;
   begin
   end;
 
-function seq( rules : array of const ) : TRule;
+function seq( rules : array of const ) : TRuleID;
   begin
   end;
 
-function rep( rules : array of const ) : TRule;
+function rep( rules : array of const ) : TRuleID;
   begin
   end;
 
-function neg( rules : array of const ) : TRule;
+function neg( rules : array of const ) : TRuleID;
   begin
   end;
 
-function opt( rules : array of const ) : TRule;
+function opt( rules : array of const ) : TRuleID;
   begin
   end;
 
-function orp( rules : array of const ) : TRule;
+function orp( rules : array of const ) : TRuleID;
   begin
   end;
 
-function def( name : string; rules : array of const ) : TRule;
+function def( name : string; rules : array of const ) : TRuleID;
   begin
   end;
 
-function sub( name : string ) : TRule;
+function sub( name : string ) : TRuleID;
   begin
   end;
 
-function act( action : cardinal; rules : array of const ) : TRule;
+function act( action : cardinal; rules : array of const ) : TRuleID;
   begin
   end;
 
-function tok( name : string; rules : array of const ) : TRule;
+function tok( name : string; rules : array of const ) : TRuleID;
   begin
   end;
 
-function skip( name : string; rules : array of const ) : TRule;
+function skip( name : string; rules : array of const ) : TRuleID;
   begin
   end;
 
-function node( name : string; rules : array of const ) : TRule;
+function node( name : string; rules : array of const ) : TRuleID;
   begin
   end;
 
-function hide(rules : array of const) : TRule;
+function hide(rules : array of const) : TRuleID;
   begin
   end;
 
-function lift(rules : array of const) : TRule;
+function lift(rules : array of const) : TRuleID;
   begin
   end;
 
-function virt( name : string ) : TRule;
+function virt( name : string ) : TRuleID;
   begin
   end;
 
