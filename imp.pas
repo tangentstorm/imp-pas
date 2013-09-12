9f62518b507ac623ab05d2b8bb0e172d211f9b1b
tangentstorm <michal.wallace@gmail.com>
2013-09-12 17:05:32 -0500

    defined symbols for the built-in functions

--- imp.pas
+++ imp.pas
@@ -42,7 +42,11 @@ type
     kSTR,  // alternate symbol syntax with quotes to allow spaces
     kINT,  // an integer
     kCEL,  // a 'cons cell' (pair of sybols)
-    kMET); // a meta-function (TExpr->TExpr, implemented in pascal)
+    kMF1,  // a meta-function (a:TExpr -> TExpr, implemented in pascal)
+    kMF2,  // a meta-function (a,b:TExpr -> TExpr)
+    kMF3,  // a meta-function (a,b,c:TExpr -> TExpr)
+    kMF4); // a meta-function (a,b,c,d:TExpr -> TExpr)
+
   TExpr = record
             kind : TKind;
             data : integer;
@@ -57,7 +61,7 @@ function Sx( kind : TKind; data : integer ) : TExpr;
 
 // - atoms - - - - - - - - - - - - - - - - - - - - - - - - - - -
 // Any s-expression where kind<>kCEL is an atom.
-const atomic = [kSYM..kMET] - [kCEL];
+const atomic = [kSYM..kMF4] - [kCEL];
 
 // We can represent Symbols, NULL, Errors, and String atoms
 // as strings in pascal, and store values for all of them in
@@ -91,13 +95,17 @@ var cells : TCellTbl;
 // McCarthy used the m-expression syntax as a meta-language to
 // describe how to evaluate S-expressions. ('m' is for 'meta')
 // Since we're using pascal for our meta language, we translate
-// m-expressions to pascal functions.
+// m-expressions to pascal functions. We will allow defining
+// variables of up to 5 arguments.
 type
-  TMetaFun = function (var expr : TExpr) : TExpr;
+  TMetaFun1 = function (a : TExpr) : TExpr;
+  TMetaFun2 = function (a,b : TExpr) : TExpr;
+  TMetaFun3 = function (a,b,c : TExpr) : TExpr;
+  TMetaFun4 = function (a,b,c,d : TExpr) : TExpr;
 
-// An s-expression of kind=kMET is therefore not an M-expression
+// An s-expression of kind=kMFx is therefore not an M-expression
 // but a symbol that represents a particular pascal function of
-// this type.
+// one of these types.
 //
 // Note that we are not limited to the functions McCarthy gave us.
 // Any pascal code can be exposed to our interpreter by creating
@@ -110,20 +118,32 @@ type
 //
 // [1] http://bugs.freepascal.org/view.php?id = 25002
 type
-  TMetaTbl = array [0..31] of TMetaFun;
+  TMetaTbl = array [byte] of pointer;
 var
   metas	    : TMetaTbl;
-  metacount : cardinal = 0;
+  metacount : byte = 0;
 
 // Meta adds a function record to the 'metas' table and constructs
 // a unique symbol for it.
-function Meta( f : TMetaFun ) : TExpr;
+function NextMeta( k : TKind; p : pointer ) : TExpr;
   begin
-    metas[metacount] := f;
+    metas[metacount] := p;
     if metacount > high(metas) then halt( 'out of meta slots.' );
-    result := Sx(kMET, metacount);
+    result := Sx(k, metacount);
     inc(metacount);
   end;
+
+function Meta( f : TMetaFun1 ) : TExpr; overload;
+  begin NextMeta(kMF1, @f) end;
+
+function Meta( f : TMetaFun2 ) : TExpr; overload;
+  begin NextMeta(kMF2, @f) end;
+
+function Meta( f : TMetaFun3 ) : TExpr; overload;
+  begin NextMeta(kMF3, @f) end;
+
+function Meta( f : TMetaFun4 ) : TExpr; overload;
+  begin NextMeta(kMF4, @f) end;
 
 //-- c. elementary meta-expressions ----------------------------
 
@@ -187,14 +207,14 @@ function MCONS( x, y : TExpr ) : TExpr;
 // the symbol NULL.
 
 var { boolean symbols }
-  mNULL : TExpr;
-  mTRUE : TExpr;
+  sNULL : TExpr;
+  sTRUE : TExpr;
 
 // We will call this routine at startup to initialize them:
 procedure CreateBooleans;
   begin
-    mNULL := Sx(kNUL, Key('()'));
-    mTRUE := Sx(kSYM, Key('T'));
+    sNULL := Sx(kNUL, Key('()'));
+    sTRUE := Sx(kSYM, Key('T'));
   end;
 
 // To translate:
@@ -202,7 +222,7 @@ procedure CreateBooleans;
 // EnBool encodes a pascal boolean as an s-expression:
 function EnBool( b : boolean ) : TExpr;
   begin
-    if b then result := mTRUE else result := mNULL
+    if b then result := sTRUE else result := sNULL
   end;
 
 // ExBool extracts a pascal boolean from an s-expression.
@@ -256,17 +276,15 @@ function MEQUAL( x, y : TExpr ) : TExpr;
   end;
 
 // 4. null?(x) -> T if x = NIL else F
-// mNULL is already the name of the constant so this
-// one breaks the naming convention a bit.
-function NULLP( x : TExpr ) : boolean;
+function MNULL( x : TExpr ) : boolean;
   begin
-    result := MEQ(x, mNULL)
+    result := MEQ(x, sNULL)
   end;
 
 // here is the reified version:
 function MNULLP( x : TExpr ) : TExpr;
   begin
-    result := MEQP(x, mNULL)
+    result := MEQP(x, sNULL)
   end;
 
 // - abbreviations - - - - - - - - - - - - - - - - - - - - - - -
@@ -300,37 +318,39 @@ function MCADDR( x : TExpr ) : TExpr;
 // people writing meta-extensions in pascal.
 
 function MLIST( a : TExpr ) : TExpr;
-  begin result := MCONS(a, mNULL) end;
+  begin result := MCONS(a, sNULL) end;
 
-// MLIST with two arguments is the same as MCONS.
+// After the first version, each successive version can simply
+// CONS its first argument onto the MLIST of the other arguments.
+
+// Note that MLIST with two arguments is *NOT* the same as MCONS.
+// (cons a (b c)) -> (a b c)
+// (list a (b c)) -> (a (b c))
 function MLIST( a, b : TExpr ) : TExpr; inline;
- begin result := MCONS(a, b) end;
+ begin result := MCONS(a, MLIST(b)) end;
 
-// After those two, each successive version can simply CONS
-// its first argument onto the MLIST of the other arguments.
-
-function MLIST( a, b, c : TExpr ) : TExpr;
+function MLIST( a, b, c : TExpr ) : TExpr; inline;
   begin result := MCONS(a, MLIST(b, c)) end;
 
-function MLIST( a, b, c, d : TExpr ) : TExpr;
+function MLIST( a, b, c, d : TExpr ) : TExpr; inline;
   begin result := MCONS(a, MLIST(b, c, d)) end;
 
-function MLIST( a, b, c, d, e : TExpr ) : TExpr;
+function MLIST( a, b, c, d, e : TExpr ) : TExpr; inline;
   begin result := MCONS(a, MLIST(b, c, d, e)) end;
 
-function MLIST( a, b, c, d, e, f : TExpr ) : TExpr;
+function MLIST( a, b, c, d, e, f : TExpr ) : TExpr; inline;
   begin result := MCONS(a, MLIST(b, c, d, e, f)) end;
 
-function MLIST( a, b, c, d, e, f, g : TExpr ) : TExpr;
+function MLIST( a, b, c, d, e, f, g : TExpr ) : TExpr; inline;
   begin result := MCONS(a, MLIST(b, c, d, e, f, g)) end;
 
-function MLIST( a, b, c, d, e, f, g, h : TExpr ) : TExpr;
+function MLIST( a, b, c, d, e, f, g, h : TExpr ) : TExpr; inline;
   begin result := MCONS(a, MLIST(b, c, d, e, f, g, h)) end;
 
-function MLIST( a, b, c, d, e, f, g, h, i : TExpr ) : TExpr;
+function MLIST( a, b, c, d, e, f, g, h, i : TExpr ) : TExpr; inline;
   begin result := MCONS(a, MLIST(b, c, d, e, f, g, h, i)) end;
 
-function MLIST( a, b, c, d, e, f, g, h, i, j : TExpr ) : TExpr;
+function MLIST( a, b, c, d, e, f, g, h, i, j : TExpr ) : TExpr; inline;
   begin result := MCONS(a, MLIST(b, c, d, e, f, g, h, i, j)) end;
 
 // - functions - - - - - - - - - - - - - - - - - - - - - - - - -
@@ -338,7 +358,7 @@ function MLIST( a, b, c, d, e, f, g, h, i, j : TExpr ) : TExpr;
 // append[x;y] -> append y to x
 function MAPPEND( x, y : TExpr ) : TExpr;
   begin
-    if NULLP(x) then result := MLIST(x)
+    if MNULL(x) then result := MLIST(x)
     else result := MCONS(MCAR(x), MAPPEND(MCDR(x), y))
   end;
 
@@ -357,7 +377,7 @@ function MAMONGP( x, y : TExpr ) : TExpr;
 // zip -- McCarthy calls this 'pair'. 'zip' comes from haskell and python.
 function MZIP( x, y : TExpr ) : TExpr;
   begin
-    if MATOM(x) or MATOM(y) then result := mNULL
+    if MATOM(x) or MATOM(y) then result := sNULL
     else result := MCONS(MLIST(MCAR(x), MCAR(y)),
                          MZIP(MCDR(x), MCDR(y)))
   end;
@@ -373,7 +393,7 @@ function MASSOC( x, y : TExpr ) : TExpr;
 function MSUBLIS( x, y : TExpr ) : TExpr;
   function SUB2( x, z : TExpr ) : TExpr;
     begin
-      if NULLP(x) then result := z
+      if MNULL(x) then result := z
       else if MEQ(MCAAR(x), z) then result := MCADAR(x)
       else result := SUB2(MCDR(x), z)
     end;
@@ -381,6 +401,201 @@ function MSUBLIS( x, y : TExpr ) : TExpr;
     if MATOM(x) then result := SUB2(x, y)
     else result := MCONS(MSUBLIS(x, MCAR(y)), MSUBLIS(x, MCDR(y)))
   end;
+
+//-- e. functions as s-expressions -----------------------------
+
+// In this section, McCarthy provides the rules for rewriting
+// m-expressions as s-expressions.
+
+// In pascal, our meta-language consitsts of the Sx()
+// function, our various meta-functions, and the variables
+// sTRUE and sNULL. We can even refer to our pascal functions
+// as symbols by adding them to the 'metas' array and creating
+// a new TExpr where kind=kMET.
+//
+// We will go ahead and create variables now for all of the routines
+// of type (TExpr -> TExpr) that we've defined so far, as well as
+// the ones we're about to define below.
+var
+  sAtomP, sEqP, sCar, sCdr, sCons, sFF, sSubst, sEqualP, sNullP,
+  sCaar, sCadr, sCadar, sCaddr, sList, sAppend, sAmongP, sZip,
+  sAssoc, sSublis : TExpr;
+
+// and the new ones...
+  sCond, sLambda, sLabel, sApply, sEval, sAppq,
+  sMapList, sSearch, sFilter, sReduce,
+  sAdd, sSub, sMul, sDiv, sMod, sLog, sDif : TExpr;
+
+// ... for which we also have to provide forward declarations:
+  function MCOND    ( x : TExpr ) : TExpr; forward;
+  function MLAMBDA  ( x, y, z : TExpr ) : TExpr; forward;
+  function MLABEL   ( x, y : TExpr ) : TExpr; forward;
+  function MAPPLY   ( f, x : TExpr ) : TExpr; forward;
+  function MEVAL    ( x : TExpr ) : TExpr; forward;
+  function MAPPQ    ( f, x : TExpr ) : TExpr; forward;
+
+  function MMAPLIST ( f, x : TExpr ) : TExpr; forward;
+  function MSEARCH  ( f, x : TExpr ) : TExpr; forward;
+  function MFILTER  ( f, x : TExpr ) : TExpr; forward;
+  function MREDUCE  ( f, x, y : TExpr ) : TExpr; forward;
+
+  function MADD ( x, y : TExpr ) : TExpr; forward;
+  function MSUB ( x, y : TExpr ) : TExpr; forward;
+  function MMUL ( x, y : TExpr ) : TExpr; forward;
+  function MDIV ( x, y : TExpr ) : TExpr; forward;
+  function MMOD ( x, y : TExpr ) : TExpr; forward;
+  function MPOW ( x, y : TExpr ) : TExpr; forward;
+  function MLOG ( x, y : TExpr ) : TExpr; forward;
+  function MDIF ( x, y : TExpr ) : TExpr; forward;
+
+// Finally, we will define one more TExpr variable, to hold
+// the current environment (the list of variable bindings).
+// It's format will be (((n0 . v1) (n1 . v1)...) ...)
+// where the outermost list contains a stack of scopes,
+// and each inner list contains name-value pairs. The
+// CAR of mENV will be the innermost scope.
+var mENV : TExpr;
+
+function Define(n:string; x:TExpr) : TExpr;
+  begin
+    result := Sx(kSYM, Key(n)); // symbol representing the name
+    // env: (((x . y) ...) ...) -> (((n . f) (x . y) ...) ...)
+    mENV := MCONS(
+      MCONS(MLIST(result, x),
+            MCAAR(mENV)),
+      MCDR(mENV));
+  end;
+
+procedure CreateBuiltins;
+  begin
+    sAtomP := Define('atom?', Meta(@MATOMP));
+    sEqP := Define('eq?', Meta(@MEQP));
+    sCar := Define('car', Meta(@MCAR));
+    sCdr := Define('cdr', Meta(@MCDR));
+    sCons := Define('cons', Meta(@MCONS));
+    sFF := Define('ff', Meta(@MFF));
+    sSubst := Define('subst', Meta(@MSUBST));
+    sEqualP := Define('equal', Meta(@MEQUAL));
+    sNullP := Define('null?', Meta(@MNULLP));
+    sCaar := Define('caar', Meta(@MCAAR));
+    sCadr := Define('cadr', Meta(@MCADR));
+    sCadar := Define('cadar', Meta(@MCADAR));
+    sCaddr := Define('caddr', Meta(@MCADDR));
+    sList := Define('list', Meta(@MLIST));
+    sAppend := Define('append', Meta(@MAPPEND));
+    sAmongP := Define('among?', Meta(@MAMONGP));
+    sZip := Define('zip', Meta(@MZIP));
+    sAssoc := Define('assoc', Meta(@MASSOC));
+    sSublis := Define('sublis', Meta(@MSUBLIS));
+    sCond := Define('cond', Meta(@MCOND));
+    sLambda := Define('lambda', Meta(@MLAMBDA));
+    sLabel := Define('label', Meta(@MLABEL));
+    sApply := Define('apply', Meta(@MAPPLY));
+    sEval := Define('eval', Meta(@MEVAL));
+    sAppq := Define('appq', Meta(@MAPPQ));
+    sMapList := Define('maplist', Meta(@MMAPLIST));
+    sSearch := Define('search', Meta(@MSEARCH));
+    sFilter := Define('filter', Meta(@MFILTER));
+    sReduce := Define('reduce', Meta(@MREDUCE));
+    sAdd := Define('add', Meta(@MADD));
+    sSub := Define('sub', Meta(@MSUB));
+    sMul := Define('mul', Meta(@MMUL));
+    sDiv := Define('div', Meta(@MDIV));
+    sMod := Define('mod', Meta(@MMOD));
+    sLog := Define('log', Meta(@MLOG));
+    sDif := Define('dif', Meta(@MDIF));
+  end;
+
+// Now let's consider each of his translation rules, in order.
+// Following McCarthy's lead, we'll use the notation E* to
+// represents the s-expression translation of m-expression E.
+
+// 1. for any s-expression E, E* is (quote E)
+var sQUOTE : TExpr;
+function Q(item:TExpr) : TExpr;
+  begin
+    result := MLIST(sQUOTE)
+  end;
+
+function MCOND( x : TExpr ) : TExpr;
+  begin
+  end;
+
+function MLAMBDA( x, y, z : TExpr ) : TExpr;
+  begin
+  end;
+
+function MLABEL( x, y : TExpr ) : TExpr;
+  begin
+  end;
+
+//-- f. universal evaluator ------------------------------------
+
+function MAPPLY( f, x : TExpr ) : TExpr;
+  begin
+  end;
+
+function MEVAL( x : TExpr ) : TExpr;
+  begin
+  end;
+
+function MAPPQ( f, x : TExpr ) : TExpr;
+  begin
+  end;
+
+//-- g. higher order functions ---------------------------------
+
+function MMAPLIST( f, x : TExpr ) : TExpr;
+  begin
+  end;
+
+function MSEARCH( f, x : TExpr ) : TExpr;
+  begin
+  end;
+
+function MFILTER( f, x : TExpr ) : TExpr;
+  begin
+  end;
+
+function MREDUCE( f, x, y : TExpr ) : TExpr;
+  begin
+  end;
+
+
+//-- arithmetic  -----------------------------------------------
+
+function MADD( x, y : TExpr ) : TExpr;
+  begin
+  end;
+
+function MSUB( x, y : TExpr ) : TExpr;
+  begin
+  end;
+
+function MMUL( x, y : TExpr ) : TExpr;
+  begin
+  end;
+
+function MDIV( x, y : TExpr ) : TExpr;
+  begin
+  end;
+
+function MMOD( x, y : TExpr ) : TExpr;
+  begin
+  end;
+
+function MPOW( x, y : TExpr ) : TExpr;
+  begin
+  end;
+
+function MLOG( x, y : TExpr ) : TExpr;
+  begin
+  end;
+
+function MDIF( x, y : TExpr ) : TExpr;
+  begin
+  end;
+
 
 // - functions - - - - - - - - - - - - - - - - - - - - - - - - -
 type
@@ -420,7 +635,10 @@ function k2s( kind :  TKind ) : string;
       kERR : result := 'ERR';
       kINT : result := 'INT';
       kSTR : result := 'STR';
-      kMET : result := 'MET';
+      kMF1 : result := 'MF1';
+      kMF2 : result := 'MF2';
+      kMF3 : result := 'MF3';
+      kMF4 : result := 'MF4';
     end
   end;
 
@@ -553,7 +771,7 @@ function ReadListEnd : TExpr;
         '(' : expect := ')';
         else expect := '?' // should never happen
       end;
-      if ch = expect then result := mNULL
+      if ch = expect then result := sNULL
       else result := Sx(kERR, Key('List end mismatch. Expected: '
                          + expect + ', got: ' + ch));
     end;
@@ -570,7 +788,7 @@ function ReadNext( out value : TExpr ): TExpr;
       SkipCommentsAndWhitespace;
       if (ch in [')', ']', '}']) then
         begin
-          res := mNULL; NextChar(ch);
+          res := sNULL; NextChar(ch);
         end
       else if ReadNext(car).kind = kERR then res := car
       else if ReadList(cdr, false).kind = kERR then res := cdr
@@ -667,7 +885,7 @@ function ShowExpr( expr : TExpr ) : string;
       kSTR : result := '"' + syms[ expr.data ] + '"';
       kINT : result := IntToStr( expr.data );
       kCEL : result := ShowList( expr, true );
-      kMET : result := '<' + IntToStr( expr.data ) + '>';
+      else result := '<' + IntToStr( expr.data ) + '>';
     end;
     if showFormat = fmtStruct then
       case expr.kind of
@@ -676,8 +894,8 @@ function ShowExpr( expr : TExpr ) : string;
         kSYM,
         kSTR : result := 's:' + result;
         kINT : result := 'n:' + result;
-        kMET : result := 'm:' + result;
         kCEL : result := ShowList( expr, true );
+        else result := 'm:' + result;
       end;
   end; { ShowExpr }
 
@@ -692,6 +910,7 @@ begin
   cells := TCellTbl.Create;
   defs := TDefTbl.Create;
   CreateBooleans;
+  CreateBuiltins;
   repeat Print(Eval(ReadNext(val)))
   until (val.kind = kERR)
 end.
